267b3d580094d8af65e25c52ab0ac80ccb93d3e2
Mani Gudvardarson <m.gudvardarson@amsterdam.nl>
2024-06-10 14:39:09 +0200

    Route defs

--- src/pages/routes.ts
+++ src/pages/routes.ts
@@ -3,13 +3,13 @@ interface IROUTE {
 }
 
 export const routes: IROUTE = {
-    'HOME': '#/',
-    'ABOUT': '#/about',
-    'SOCIALS': '#/socials',
-    'IDEENBAK': '#/ideenbak',
-    'START_MOKUMMATCH': '#/mokummatch',
-    'QUESTIONNAIRE': '#/mokummatch/questionnaire/:id',
-    'TOP_THREE':'#/mokummatch/topthree'
+    'HOME': '/',
+    'ABOUT': '/about',
+    'SOCIALS': '/socials',
+    'IDEENBAK': '/ideenbak',
+    'START_MOKUMMATCH': '/mokummatch',
+    'QUESTIONNAIRE': '/mokummatch/questionnaire/:id',
+    'TOP_THREE':'/mokummatch/topthree'
 }
 
 export const getRouteById = (route: string, id: any) =>{
